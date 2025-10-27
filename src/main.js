3bb6464749a3a9a54f158c1911f310ce8e13f560
Rosomak2024 <mbnet@o2.pl>
2025-10-27 12:01:05 +0100

    error img fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import "./style.css";
 document.querySelector("#app").innerHTML = `
 <section class="web_section">
     <h1>Web Developer</h1>
-    <img href="./assets/FB_IMG_1630259240524.jpg" alt="karateka_img"></img>
+    <img class="karateka_img" src="./assets/FB_IMG_1630259240524.jpg" alt="karateka_img"/>
 </section>
 <section class="traine_section">
     <h2>Personal MMA Trainer</h2>
