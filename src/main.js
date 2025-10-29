ee0955480ac92667676c3d1c54cd703af2286f1d
Rosomak2024 <mbnet@o2.pl>
2025-10-29 11:35:50 +0100

    clsses hide and show fixed an listeners added

--- src/main.js
+++ src/main.js
@@ -1,30 +1,39 @@
 import "./style.css";
 
 document.querySelector("#app").innerHTML = `
-<section class="dev_section">
-    <h1>Web Developer</h1>
-    <img class="dev_img" src="./assets/logo_web_light.png" alt="dev_img"></img>
-    </section>
-    <section class="traine_section">
-    <h2>Personal MMA Trainer</h2>
-    <img class="traine_img" src="./assets/FB_IMG_1630259240524.jpg" alt="karateka_img"/>
+<section class="dev_section_show">
+  <img class="dev_img" src="./assets/logo_web_light.png" alt="Web Developer Logo"></img>
+  <h1>Web Developer</h1>
+</section>
+<section class="dev_page_hide">
+  <header>
+    <nav>nawigacja</nav>
+  </header>
+  <button class="btn_back">Wróć</button>
+    Strona o web deveploer
+  <footer>stopka</footer>
+</section>
+<section class="traine_section_show">
+  <img class="traine_img" src="./assets/FB_IMG_1630259240524.jpg" alt="Personal MMA Trainer Image"/>
+  <h2>Personal MMA Trainer</h2>
 </section>`;
 
-const dev_page_change = document.querySelector(".traine_img");
-const traine_page_change = document.querySelector(".dev_section");
+const devPagePhoto = document.querySelector(".dev_img");
+const btnBack = document.querySelector(".btn_back");
+const devPage = document.querySelector(".dev_page_hide");
+const devIntro = document.querySelector(".dev_section_show");
+const traineIntro = document.querySelector(".traine_section_show");
 
-dev_page_change.addEventListener("click", function () {
-  document.querySelector("#app").innerHTML = `<header>
-        <nav>nawigacja</nav>
-      </header>
-      <section>Strona o web deveploer</section>
-      <footer>stopka</footer>`;
+devPagePhoto.addEventListener("click", function () {
+  devPage.classList.replace("dev_page_hide", "dev_page_show");
+  devIntro.classList.replace("dev_section_show", "dev_section_hide");
+  traineIntro.classList.replace("traine_section_show", "traine_section_hide");
 });
 
-traine_page_change.addEventListener("click", function () {
-  document.querySelector("#app").innerHTML = `<header>
-          <nav>nawigacja</nav>
-        </header>
-        <section>Strona o personal trainer</section>
-        <footer>stopka</footer>`;
+// traine_page_change.addEventListener("click", function () {});
+
+btnBack.addEventListener("click", function () {
+  devIntro.classList.replace("dev_section_hide", "dev_section_show");
+  traineIntro.classList.replace("traine_section_hide", "traine_section_show");
+  devPage.classList.replace("dev_page_show", "dev_page_hide");
 });
