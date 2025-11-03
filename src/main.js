8b2e826854145c3c2e73dfcdbbeb582edcf2eb43
Rosomak2024 <mbnet@o2.pl>
2025-11-03 12:03:28 +0100

    dynamic button added

--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
 import "./style.css";
-
-document.querySelector("#app").innerHTML = `
-<section class="dev_section_show">
-  <img class="dev_img" src="./assets/logo_web_light.png" alt="Web Developer Logo"></img>
-  <h1>Web Developer</h1>
-</section>
-<section class="dev_page_hide">
-  <header>
-    <nav>nawigacja</nav>
-  </header>
-  <button class="btn_back">Wróć</button>
-    Strona o web deveploer
-  <footer>stopka</footer>
-</section>
-<section class="traine_section_show">
-  <img class="traine_img" src="./assets/FB_IMG_1630259240524.jpg" alt="Personal MMA Trainer Image"/>
-  <h2>Personal MMA Trainer</h2>
-</section>`;
+// import "./styles/dev_page.css";
 
 const devPagePhoto = document.querySelector(".dev_img");
-const btnBack = document.querySelector(".btn_back");
+const trainePagePhoto = document.querySelector(".traine_img");
 const devPage = document.querySelector(".dev_page_hide");
+const trainePage = document.querySelector(".traine_page_hide");
 const devIntro = document.querySelector(".dev_section_show");
 const traineIntro = document.querySelector(".traine_section_show");
 
@@ -28,12 +12,37 @@ devPagePhoto.addEventListener("click", function () {
   devPage.classList.replace("dev_page_hide", "dev_page_show");
   devIntro.classList.replace("dev_section_show", "dev_section_hide");
   traineIntro.classList.replace("traine_section_show", "traine_section_hide");
+
+  const button = document.createElement("button");
+  devPage.appendChild(button);
+  button.classList.add("btn_back");
+  button.innerText = "Wróć";
+
+  const btnBack = document.querySelector(".btn_back");
+  btnBack.addEventListener("click", function () {
+    devIntro.classList.replace("dev_section_hide", "dev_section_show");
+    traineIntro.classList.replace("traine_section_hide", "traine_section_show");
+    devPage.classList.replace("dev_page_show", "dev_page_hide");
+    btnBack.remove();
+  });
 });
 
-// traine_page_change.addEventListener("click", function () {});
+trainePagePhoto.addEventListener("click", function () {
+  trainePage.classList.replace("traine_page_hide", "traine_page_show");
+  devIntro.classList.replace("dev_section_show", "dev_section_hide");
+  traineIntro.classList.replace("traine_section_show", "traine_section_hide");
 
-btnBack.addEventListener("click", function () {
-  devIntro.classList.replace("dev_section_hide", "dev_section_show");
-  traineIntro.classList.replace("traine_section_hide", "traine_section_show");
-  devPage.classList.replace("dev_page_show", "dev_page_hide");
+  const button = document.createElement("button");
+  trainePage.appendChild(button);
+  button.classList.add("btn_back");
+  button.innerText = "Wróć";
+
+  const btnBack = document.querySelector(".btn_back");
+  btnBack.addEventListener("click", function () {
+    console.log("działam działam w dev");
+    devIntro.classList.replace("dev_section_hide", "dev_section_show");
+    traineIntro.classList.replace("traine_section_hide", "traine_section_show");
+    trainePage.classList.replace("traine_page_show", "traine_page_hide");
+    btnBack.remove();
+  });
 });
